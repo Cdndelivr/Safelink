5f0a05c789d9a9e1620bf66b2d4cde7bebaf7bb7
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-11-01 07:31:13 +0530

    Update fetchUrl.js

--- fetchUrl.js
+++ fetchUrl.js
@@ -1,17 +1,9 @@
-var feedUrl = "/feeds/posts/default?alt=json&max-results=1000";
-var links = [];
+const feedUrl = "/feeds/posts/default?alt=json&max-results=1000";
+const links = [];
 
 fetch(feedUrl)
   .then(response => response.json())
   .then(data => {
-    var entries = data.feed.entry;
-    if (entries) {
-      entries.forEach(entry => {
-        var url = entry.link.find(link => link.rel === "alternate").href;
-        links.push(url);
-      });
-    }
+    links.push(...(data.feed.entry || []).map(entry => entry.link.find(link => link.rel === "alternate").href));
   })
- .catch(error => {
-    console.error("Error fetching Posts:");
-  });
+  .catch(error => console.error("Error fetching Posts:"));
