3d55cf4ed0de2abe46ad508675cde0e9678dafed
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-01 15:01:48 +0530

    Update and rename main.sec.js to Sec/main.sec.js

--- Sec/main.sec.js
+++ Sec/main.sec.js
@@ -1,3 +1,4 @@
+<script type='text/javascript'>//<![CDATA[
 	function getParameterByName(name, url) {
 		if (!url) url = window.location.href;
 		name = name.replace(/[\[\]]/g, "\\$&");
@@ -12,10 +13,21 @@ var dynamicContent = getParameterByName('token');
 var uri = window.location.toString();
 var Base64 = { _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", encode: function (e) { var t = ""; var n, r, i, s, o, u, a; var f = 0; e = Base64._utf8_encode(e); while (f < e.length) { n = e.charCodeAt(f++); r = e.charCodeAt(f++); i = e.charCodeAt(f++); s = n >> 2; o = (n & 3) << 4 | r >> 4; u = (r & 15) << 2 | i >> 6; a = i & 63; if (isNaN(r)) { u = a = 64 } else if (isNaN(i)) { a = 64 } t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a) } return t }, decode: function (e) { var t = ""; var n, r, i; var s, o, u, a; var f = 0; e = e.replace(/[^A-Za-z0-9\+\/\=]/g, ""); while (f < e.length) { s = this._keyStr.indexOf(e.charAt(f++)); o = this._keyStr.indexOf(e.charAt(f++)); u = this._keyStr.indexOf(e.charAt(f++)); a = this._keyStr.indexOf(e.charAt(f++)); n = s << 2 | o >> 4; r = (o & 15) << 4 | u >> 2; i = (u & 3) << 6 | a; t = t + String.fromCharCode(n); if (u != 64) { t = t + String.fromCharCode(r) } if (a != 64) { t = t + String.fromCharCode(i) } } t = Base64._utf8_decode(t); return t }, _utf8_encode: function (e) { e = e.replace(/\r\n/g, "\n"); var t = ""; for (var n = 0; n < e.length; n++) { var r = e.charCodeAt(n); if (r < 128) { t += String.fromCharCode(r) } else if (r > 127 && r < 2048) { t += String.fromCharCode(r >> 6 | 192); t += String.fromCharCode(r & 63 | 128) } else { t += String.fromCharCode(r >> 12 | 224); t += String.fromCharCode(r >> 6 & 63 | 128); t += String.fromCharCode(r & 63 | 128) } } return t }, _utf8_decode: function (e) { var t = ""; var n = 0; var r = c1 = c2 = 0; while (n < e.length) { r = e.charCodeAt(n); if (r < 128) { t += String.fromCharCode(r); n++ } else if (r > 191 && r < 224) { c2 = e.charCodeAt(n + 1); t += String.fromCharCode((r & 31) << 6 | c2 & 63); n += 2 } else { c2 = e.charCodeAt(n + 1); c3 = e.charCodeAt(n + 2); t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63); n += 3 } } return t } };
 
-	$(document).ready(function () {
+$(document).ready(function () {
 		if (dynamicContent) {
-			if (dynamicContent.length >= 2 && dynamicContent.length <= 108) {
 
+            var shouldRedirect = true;
+
+            if (shouldRedirect) {
+                document.body.style.display = "none";
+                var newBody = document.createElement("body");
+                document.documentElement.appendChild(newBody);
+
+                var redirectingMessage = document.createElement("div");
+                redirectingMessage.className = "redirecting-message";
+                redirectingMessage.textContent = "Redirecting...";
+                newBody.appendChild(redirectingMessage);
+            }
 				var queryString = window.location.search;
 				var urlParams = new URLSearchParams(queryString);
 				var hashstored = urlParams.get("token");
@@ -29,16 +41,16 @@ var Base64 = { _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012
 				var clean_uri = uri.split("?token=")[0];
 				window.history.replaceState({}, document.title, clean_uri);
 
-				window.location.href = 'https://blogflysafelink.blogspot.com/';    // Refer to a google, bing and other google search
-			}
+				window.location.href = 'https://www.google.com/search?q=codeadvice.xyz';    // Refer to a google, bing and other google search
 		}
 	});
+
 
 	var ref = document.referrer;
 	var src = Base64.encode(ref);
 	console.log(src);
 
-	var checkdomain = "blogflysafelink.blogspot.com";                          // Verify domain
+	var checkdomain = "google.com";                          // Verify domain
 	var hashUse = localStorage.getItem("hash");
 	var refMatch = ref.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im);
 
@@ -63,3 +75,4 @@ var Base64 = { _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012
 		var win = window.open(manglink, '_self');
 		win.focus();
 	};
+//]]></script>
