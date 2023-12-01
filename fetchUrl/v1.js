d7e932471465a7f1ef263b793384a2dacac1f2ec
Tech Advice <60535290+Cdndelivr@users.noreply.github.com>
2023-12-01 10:17:48 +0530

    Create v1.js

---
+++ fetchUrl/v1.js
@@ -0,0 +1,19 @@
+<script type='text/javascript'>
+var feedUrl = "/feeds/posts/default?alt=json&max-results=1000";
+var links = [];
+
+fetch(feedUrl)
+  .then(response => response.json())
+  .then(data => {
+    var entries = data.feed.entry;
+    if (entries) {
+      entries.forEach(entry => {
+        var url = entry.link.find(link => link.rel === "alternate").href;
+        links.push(url);
+      });
+    }
+  })
+ .catch(error => {
+    console.error("Error fetching Posts:");
+  });
+</script>
